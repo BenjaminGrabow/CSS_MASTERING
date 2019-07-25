0f7db79775835625bac2d0c17ef0ef56fb29777f
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-07-25 19:09:44 +0200

    Feature: create-react-app and install styled-components

---
+++ natours/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+import './App.css';
+
+const StyledHeader = styled.div`
+
+`;
+
+function App() {
+  return (
+    <StyledHeader>
+     
+    </StyledHeader>
+  );
+}
+
+export default App;
